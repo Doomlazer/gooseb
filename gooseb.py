e426f0cf27ad25d759017ad4151a8a6760521c31
DL <dl@thespr3.com>
2024-11-03 16:11:13 -0800

    clean up

--- gooseb.py
+++ gooseb.py
@@ -2,25 +2,23 @@ import os
 import json
 import random
 # Text to Speech requires:
-#   pip install pyttsx4
-# Also had to do:
 #   pip install py3-tts
-try:
-    import pyttsx4                    
-except ImportError:
-    print("failed to load TTS")
-    noSpeech = True
+import pyttsx3                   
 
-noSpeech = False
+speak = False
 pageStack = []
+data = []
+tts = None
 
-def doPage(data,page,tts):
+def doPage(page):
+    global speak
+    global data
     # banner text
     os.system('clear')
     print(str('\x1b[31m') + str(data["banner"]))
     print(str('\x1b[36m') + data["title"] + str('\x1b[35m') + " Page: " + str(page) + str('\x1b[0m') + str("\n"))
     print(data[str(page)])
-    if (not noSpeech):
+    if speak:
         tts.say(data[str(page)])
         tts.runAndWait()
     # add dice roll
@@ -28,19 +26,27 @@ def doPage(data,page,tts):
         r1 = random.randint(1, 6)
         r2 = random.randint(1, 6)
         print('\x1b[37m' + f"Rolled {r1} & {r2}. Total: {(r1 + r2)}" + str('\x1b[0m\n'))
-        if (not noSpeech):
+        if speak:
             tts.say(f"Rolled {r1} & {r2}. Total: {(r1 + r2)}")
             tts.runAndWait()
     # get input
     np = input(str('\x1b[36m') + "Enter page # or help: " + str('\x1b[0m'))
     try: 
         if (str(np).lower() == "quit" or str(np).lower() == "q"):
+            # exit
             quit()
+        elif (str(np).lower() == "tts"):
+            # text to speech toggle
+            next = page
+            speak = False if speak else True
         elif ((str(np).lower() == "prev" or str(np).lower() == "p") and len(pageStack) > 0):
+            # step backward in page history
             next = pageStack.pop()
         elif (str(np).lower() in data["namedPages"]):
+            # matched namedPages
             next = np
         elif (int(np) > 0 and int(np) <= data["length"]):
+            # entry bounds
             next = np
         else:
             next = page
@@ -48,25 +54,22 @@ def doPage(data,page,tts):
         next = page
     if (not str(np).lower() == "prev") or (not str(np).lower() == "p"):
         pageStack.append(page)
-    doPage(data, next, tts)
+    doPage(next)
 
 
 def run():
-    engine = pyttsx4.init()
+    global noSpeech
+    global data
+    global tts
+    tts = pyttsx3.init()
     books = os.listdir("./books")
     if len(books) < 1:
         print("No books found. Add .JSON files to 'books' folder and try again")
         quit()
     else:
         os.system('clear')
-        print('\x1b[37m' + "Give Yourself GooseBumps Book Reader Text to Speech")
-        if (not noSpeech):
-            engine.say("Give Yourself GooseBumps Book Reader. Text to speech edition")
-            engine.runAndWait()
+        print('\x1b[37m' + "Give Yourself GooseBumps Book Reader")
         print("By DoomLazer")
-        if (not noSpeech):
-            engine.say("By Doom Laser")
-            engine.runAndWait()
         print("Version A1.0 Nov. 2024")
         print(str('\x1b[0m'))
         i = 1
@@ -83,7 +86,7 @@ def run():
             b = int(b) - 1
             with open('./books/' + str(books[b])) as f:
                 data = json.load(f, strict=False)
-            doPage(data, data["firstPage"], engine)
+            doPage(data["firstPage"])
         except ValueError:
             print("Selection too spooky; killing program...")
             quit()
